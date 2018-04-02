9975afadf07798822efac5086e4f16d5767e2b4c
TimurSokolov <timur_sokolov@epam.com>
2018-04-02 19:52:32 +0400

    Fix

--- src/main/java/com/epam/rd/TodoServiceApplication.java
+++ src/main/java/com/epam/rd/TodoServiceApplication.java
@@ -2,16 +2,9 @@ package com.epam.rd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoServiceApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TodoServiceApplication.class);
-    }
+public class TodoServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoServiceApplication.class, args);
