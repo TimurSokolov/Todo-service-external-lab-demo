1efc12f88634135b734f4400aeb171036c058ee0
Timur_Sokolov <sokolov.tima@gmail.com>
2018-04-10 00:34:59 +0400

    Валидация

--- src/main/java/com/epam/rd/controller/AuthController.java
+++ src/main/java/com/epam/rd/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,7 +43,7 @@ public class AuthController {
 	}
 
 	@PostMapping("registration")
-	public ModelAndView registration(ModelAndView modelAndView, User user, BindingResult result) {
+	public ModelAndView registration(ModelAndView modelAndView, @Validated User user, BindingResult result) {
 		modelAndView.setViewName("registration");
 
 		if (result.hasErrors()) {
